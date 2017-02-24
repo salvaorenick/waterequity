3973e9600e3fe16f3ce0a3c64c1fd8b5787d08c7
Will & Grail <admins@willgrail.com>
2017-02-24 15:50:31 -0500

    stripe error handling

--- django_project/apps/loans/views.py
+++ django_project/apps/loans/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 import stripe
+
+import json
 
 from django.conf import settings
 
@@ -10,20 +12,28 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 def createLoanView(request):
     if request.method != 'POST':
 	return HttpResponse(status=404)
-
+    
     try:
-        charge = stripe.Charge.create(
-            amount=request.POST['amount'],
-            source=request.POST['stripeToken'],
-            currency='usd',
-            metadata={
+	amount = request.POST['amount']
+
+        stripe.Charge.create(
+            amount = str(int(amount)*100),
+            source = request.POST['stripeToken'],
+            currency = 'usd',
+            metadata = {
                 'loan-option':request.POST['loan-option'],
                 'accept-terms':request.POST['accept-terms'],
             }
         )
 
-    except Exception:
-        return HttpResponse(status=500)
-        # return self.form_invalid(form)
+    # Stripe errors. see https://stripe.com/docs/api/python#handling-errors
+    except stripe.error.CardError as e:
+    	body = e.json_body
+    	err  = body['error']
+    	return JsonResponse({'message': err['message']}, status=400)
+
+     # system errors	
+    except Exception as e:
+        return JsonResponse({'message': {'error': {'message': 'There was an issue with the system. Please try again later. Thank you.'}}}, status=501)
 
     return HttpResponse(status=200)

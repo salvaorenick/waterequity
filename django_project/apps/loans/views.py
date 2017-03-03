362ac121638554330dfb7a952cb5a43a6bafd697
Nathan Victor <nathanqvictor@gmail.com>
2017-03-03 15:56:35 -0600

    adding customer to charge - oops. make pep8 mostly happy.

--- django_project/apps/loans/views.py
+++ django_project/apps/loans/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, JsonResponse
 from django.conf import settings
 
 import stripe
-import json
+
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -12,17 +12,17 @@ def createLoanView(request):
         return HttpResponse(status=404)
 
     try:
+        amount = str(int(request.POST['amount']) * 100)
 
         customer = stripe.Customer.create(
-            email = request.POST['email-address'],
-            source = request.POST['stripeToken'],
+            email=request.POST['email-address'],
+            source=request.POST['stripeToken'],
         )
 
-        amount = request.POST['amount']
-
-        charge = stripe.Charge.create(
-            amount = str(int(amount) * 100),
+        stripe.Charge.create(
+            amount=amount,
             currency='usd',
+            customer=customer.id,
             metadata={
                 'loan-option': request.POST['loan-option'],
                 'accept-terms': request.POST['accept-terms'],
@@ -36,7 +36,7 @@ def createLoanView(request):
         return JsonResponse({'message': err['message']}, status=400)
 
     # System errors
-    except Exception as e:
+    except Exception:
         return JsonResponse({'message': 'There was an issue with the system. Please try again later. Thank you.'}, status=501)
 
     return HttpResponse(status=200)
